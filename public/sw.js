e93581ff8ef2f8d6564ddb537295f2c9d8bab38b
Yad-cam <2021171017@uteq.edu.mx>
2024-02-12 17:50:45 -0600

    app-shell

--- public/sw.js
+++ public/sw.js
@@ -1,11 +1,19 @@
 
-self.addEventListener("fetch", function (event) {
-  const offlineResponse = fetch("pages/Offline.html")
- 
-  
-    const resp = fetch(event.request);
-    event.respondWith(resp.catch(() => offlineResponse));
-  
-    event.respondWith(resp);
-  });
+self.addEventListener('install', function(event) {
+    const cacheAppShell = caches.open('cache-v2').then(function(cache) {
+        return cache.addAll([
+            '/',
+            '/index.html',
+            '/js/app.js',
+            "/sw.js",
+            "static/js/bundle.js",
+            "favicon.ico",
+        ]);
+    }
+    );
+    event.waitUntil(cacheAppShell);
+});
 
+self.addEventListener('fetch', function(event) {
+    event.respondWith(caches.match(event.request))
+})
--- src/App.js
+++ src/App.js
@@ -1,18 +1,3 @@
-import React from 'react';
-import boston1 from './assets/boston1.jpeg';
-import boston2 from './assets/boston2.jpeg';
-import boston3 from './assets/boston3.jpeg';
-
-import './App.css';
-
-const App = () => (
-  <div className='App'>
-    <h1>Respuesta offline</h1>
-    <div className='image-container'>
-      <img src={boston1} alt="boston" className='image' />
-      <img src={boston2} alt="boston" className='image' />
-      <img src={boston3} alt="boston" className='image' />
-    </div>
-  </div>
-);
-export default App;
+if (navigator.serviceWorker) {
+    navigator.serviceWorker.register('/sw.js')
+}
